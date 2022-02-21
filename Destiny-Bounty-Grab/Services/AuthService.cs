230b4b917dd4c677a716ee0187c51ffac775f9ac
TomG1993 <gorton.tom@hotmail.co.uk>
2022-02-21 17:32:29 +0000

    Working on Auth

---
+++ Destiny-Bounty-Grab/Services/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Destiny_Bounty_Grab.Services
+{
+    public class AuthService
+    {
+        public async void Auth(string authUri)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-Key", "7fa7a3ab0ffb44308e112a6ba16ec7c2");
+
+                //GET https://www.bungie.net/en/oauth/authorize?client_id=12345&response_type=code&state=6i0mkLx79Hp91nzWVeHrzHG4
+
+                var response = await client.GetAsync(authUri + "authorize?ClientId=39385&response_type=code&state=2558test");
+                var query = response.RequestMessage.RequestUri;
+
+                var queryDecode = HttpUtility.UrlDecode(query.Query);
+                var content = response.Content.ReadAsStringAsync();
+                //dynamic item = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+
+                //Console.WriteLine(item.Response.data.inventoryItem.itemName); //Gjallarhorn
+            }
+        }
+    }
+}
--- Destiny-Bounty-Grab/Services/UserService.cs
+++ Destiny-Bounty-Grab/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Destiny_Bounty_Grab.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace Destiny_Bounty_Grab.Services
 {
-    internal class UserService
+    public class UserService
     {
+       public async Task<List<CharacterData>> GetCharacters(string destinyUri)
+        {
+            /// Destiny2 /{ membershipType}/ Profile /{ destinyMembershipId}/
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-Key", "7fa7a3ab0ffb44308e112a6ba16ec7c2");
+
+                var response = client.GetAsync(destinyUri + "1/profile/4611686018430018099?Components=Characters").Result;
+                var profileResult = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    var item = Newtonsoft.Json.JsonConvert.DeserializeObject<ProfileCharacterRoot>(profileResult);
+                    return item.Response.characters.data.Values.ToList();
+                }
+                catch (Exception e)
+                {
+                    var test = e.Data;
+                    throw;
+                }
+
+
+                return null; 
+            }
+        }
     }
 }
